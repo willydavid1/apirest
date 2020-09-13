a45a66c41e0ec796752d6120f875a2aaddb70fbd
Willy Da Conceicao <willydavidlozada@gmail.com>
2020-09-13 13:36:19 -0500

    try/catch async-await

--- src/controllers/products.controller.js
+++ src/controllers/products.controller.js
@@ -2,20 +2,27 @@ import Product from '../models/Product'
 
 export const createProduct = async (req, res) => {
   const { name, category, price, imgURL } = req.body
-
-  const newProduct = new Product({
-    name,
-    category,
-    price,
-    imgURL
-  })
-  const productSaved = await newProduct.save()
-  res.status(201).json(productSaved)
+  try {
+    const newProduct = new Product({
+      name,
+      category,
+      price,
+      imgURL
+    })
+    const productSaved = await newProduct.save()
+    res.status(201).json(productSaved)
+  } catch (error) {
+    res.status(500).json({ error: 'Error' })
+  }
 }
 
 export const getProducts = async (req, res) => {
-  const products = await Product.find()
-  res.json(products)
+  try {
+    const products = await Product.find()
+    res.json(products)
+  } catch (error) {
+    res.status(500).json({ error: 'Error' })
+  }
 }
 
 export const getProductById = async (req, res) => {
@@ -28,10 +35,12 @@ export const getProductById = async (req, res) => {
 }
 
 export const updateProductById = async (req, res) => {
-  const updatedProduct = await Product.findByIdAndUpdate(req.params.productId, req.body, {
-    new: true
-  })
-  res.status(200).json(updatedProduct)
+  try {
+    const updatedProduct = await Product.findByIdAndUpdate(req.params.productId, req.body, { new: true })
+    res.status(200).json(updatedProduct)
+  } catch (error) {
+    res.status(500).json({ error: 'Error' })
+  }
 }
 
 export const deleteProductById = async (req, res) => {
